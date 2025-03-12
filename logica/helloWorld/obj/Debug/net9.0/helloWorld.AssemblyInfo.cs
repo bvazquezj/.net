1be5397ef5d06ace6d932858c758214bf6f57156
Brandon Vazquez <bvazquezj1700@gmail.com>
2025-03-12 09:44:28 -0600

    add dataSet example

--- logica/helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
+++ logica/helloWorld/obj/Debug/net9.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b269efeffddb7db9afb3d83025cd00a1402e8839")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
