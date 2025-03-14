7ba49904a71554ca1fd597e3c93e1f696652e35b
Brandon Vazquez <bvazquezj1700@gmail.com>
2025-03-14 09:05:27 -0600

    update files

--- logica/AgendApp/obj/Debug/net9.0/AgendApp.AssemblyInfo.cs
+++ logica/AgendApp/obj/Debug/net9.0/AgendApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b269efeffddb7db9afb3d83025cd00a1402e8839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be5397ef5d06ace6d932858c758214bf6f57156")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
